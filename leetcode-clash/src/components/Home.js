bae23eba7e04e6482b9e65466008bd0314401be0
RaymondJones <Rayjones2170@gmail.com>
2024-03-31 16:59:27 -0400

    Adds functionality to lambda functions for joining and creating rooms

--- leetcode-clash/src/components/Home.js
+++ leetcode-clash/src/components/Home.js
@@ -1,10 +1,42 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // Ensure useNavigate is imported
-
+import { useEffect } from 'react';
 function Home() {
   const [roomCode, setRoomCode] = useState('');
   const [userName, setUserName] = useState('');
   const navigate = useNavigate(); // Correctly call useNavigate here
+
+  const createRoom = async () => {
+    const ws = new WebSocket('wss://rktndgn0fd.execute-api.us-east-1.amazonaws.com/Prod'); // Update with your API Gateway WebSocket URL
+
+    ws.onopen = () => {
+      console.log('WebSocket Connected');
+      ws.send(JSON.stringify({
+        action: 'createRoom', // This should match the routing in your WebSocket API
+        userName: userName, // Optional: if you want to track who created the room
+      }));
+    };
+
+    ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      if (data.success && data.roomCode) {
+        console.log("Room created successfully:", data.roomCode);
+        setRoomCode(data.roomCode); // Update the room code state with the new room
+        // Navigate to the new room, or display the room code to the user
+        // navigate('/room', { state: { roomCode: data.roomCode } });
+        ws.close(); // Close WebSocket connection after receiving the response
+      } else {
+        console.error('Error creating room:', data.message || 'Unknown error');
+        ws.close();
+      }
+    };
+
+    ws.onerror = (error) => {
+      console.log('WebSocket Error:', error);
+      ws.close();
+    };
+  };
+
 
   const joinRoom = async () => {
     // Example purpose: Simulated response for success
@@ -39,7 +71,9 @@ function Home() {
         onChange={(e) => setUserName(e.target.value)}
         placeholder="Username"
       />
-      <button onClick={joinRoom}>Enter</button> {/* Use joinRoom function on click */}
+      <button onClick={joinRoom}>Join a Room</button> {/* Use joinRoom function on click */}
+
+      <button onClick={createRoom}>Create A Room</button> {/* Use joinRoom function on click */}
     </div>
   );
 }
--- other_funcs/connect.js
+++ other_funcs/connect.js
@@ -3,7 +3,7 @@ const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 
 exports.handler = async (event) => {
-  const tableName = process.env.TABLE_NAME; // Ensure to set this environment variable in your Lambda function
+  const tableName = "TwitchWebSocketConnections"; // Ensure to set this environment variable in your Lambda function
   const connectionId = event.requestContext.connectionId;
 
   const params = {
